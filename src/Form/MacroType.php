5f7ee124f6e52d2d13669072c8767269aba55f81
Emilie Rollandin <info@archistico.com>
2023-12-09 18:05:10 +0100

    gestione categorie macro

--- src/Form/MacroType.php
+++ src/Form/MacroType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Macro;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,23 @@ class MacroType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nome')
-            ->add('Invio')
+            ->add('Nome', TextType::class, [
+                'label' => 'Nome', 
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Inserire la macro categoria'
+                ]
+            ])
+            ->add('Invio', CheckboxType::class, [
+                'label' => 'Invio al commercialista', 
+                'label_attr' => ['class' => 'form-check-label'],
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ]
+            ])
         ;
     }
 
