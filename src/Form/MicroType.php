bce456ffe8a44727baff0dbcf675f2df4c7ea343
Emilie Rollandin <info@archistico.com>
2023-12-09 21:48:12 +0100

    categorie Micro

--- src/Form/MicroType.php
+++ src/Form/MicroType.php
@@ -2,10 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Macro;
 use App\Entity\Meso;
 use App\Entity\Micro;
+use App\Repository\MacroRepository;
+use App\Repository\MesoRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +19,38 @@ class MicroType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nome')
-            ->add('Invio')
+            ->add('Nome', TextType::class, [
+                'label' => 'Nome', 
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Inserire la macro categoria'
+                ]
+            ])
+            ->add('Invio', CheckboxType::class, [
+                'label' => 'Invio al commercialista', 
+                'label_attr' => ['class' => 'form-check-label'],
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ]
+            ])
             ->add('Padre', EntityType::class, [
                 'class' => Meso::class,
-'choice_label' => 'id',
+                'expanded' => false,
+                'multiple' => false,
+                'label' => 'Macro | Meso',
+                'choice_label' => function ($f) {
+                    return $f->getPadre()->getNome() . " | " . $f->getNome() ;
+                },
+                'query_builder' => function (MesoRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.Nome', 'ASC');
+                },
+                'attr' => [
+                    'class' => 'form-control',
+                ]
             ])
         ;
     }
