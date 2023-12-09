4f01490216929a7b00f40392270b9f763d565ee2
Emilie Rollandin <info@archistico.com>
2023-12-09 23:50:25 +0100

    join categorie

--- src/Form/MovimentoType.php
+++ src/Form/MovimentoType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Micro;
+use App\Entity\Meso;
 use App\Entity\Movimento;
 use App\Entity\TipoPagamento;
+use App\Repository\MesoRepository;
+use App\Repository\MicroRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -12,6 +15,8 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\Query\Expr\Join;
+
 
 class MovimentoType extends AbstractType
 {
@@ -28,7 +33,23 @@ class MovimentoType extends AbstractType
             ])            
             ->add('Categoria', EntityType::class, [
                 'class' => Micro::class,
-                'choice_label' => 'Nome',
+                'expanded' => false,
+                'multiple' => false,
+                'label' => 'Categoria',                
+                'choice_label' => function ($f) {
+                    return $f->getPadre()->getPadre()->getNome() . " | " . $f->getPadre()->getNome() . " | " . $f->getNome() ;
+                },
+                'query_builder' => function (MicroRepository $er) {
+                    return $er->createQueryBuilder('mi')
+                        ->leftJoin('mi.Padre', 'me')
+                        ->leftJoin('me.Padre', 'ma')
+                        ->OrderBy('ma.Nome', 'ASC')
+                        ->addOrderBy('me.Nome', 'ASC')
+                        ->addOrderBy('mi.Nome', 'ASC');
+                },
+                'attr' => [
+                    'class' => 'form-control',
+                ]
             ])
             ->add('Importo', NumberType::class, [
                 'label' => 'Importo', 
@@ -43,8 +64,6 @@ class MovimentoType extends AbstractType
                 'choice_label' => 'Descrizione',
             ])
             ->add('Note')
-            
-            
         ;
     }
 
