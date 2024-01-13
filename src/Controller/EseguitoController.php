e7fdaaca4e12f3872f9e45ab0a966ae03070e036
Emilie Rollandin <info@archistico.com>
2024-01-13 17:38:15 +0100

    Eseguito elementi base

---
+++ src/Controller/EseguitoController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\EseguitoType;
+use App\Repository\EseguitoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class EseguitoController extends AbstractController
+{
+    #[Route('/admin/eseguito', name: 'app_eseguito_lista')]
+    public function Lista(EseguitoRepository $repo): Response
+    {
+        $elementi = $repo->findAll();
+
+        return $this->render('eseguito/lista.html.twig', [
+            'lista' => $elementi,
+        ]);
+    }
+
+    #[Route('/admin/eseguito/nuovo', name: 'app_eseguito_nuovo')]
+    public function Nuovo(Request $request, EntityManagerInterface $em): Response
+    {
+        $elemento = null;
+        $form = $this->createForm(EseguitoType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $elemento = $form->getData();
+            
+            $em->persist($elemento);
+            $em->flush();
+            
+            $this->addFlash('success', "Una nuova scadenza è stata eseguita");
+
+            return $this->redirectToRoute('app_eseguito_lista');
+        }
+
+        if($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', "Ci sono degli errori nell'inserimento della scadenza eseguita");
+        }
+
+        return $this->render('eseguito/nuovo.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/eseguito/modifica/{id}', name: 'app_eseguito_modifica')]
+    public function Modifica($id, Request $request, EntityManagerInterface $em, EseguitoRepository $repo): Response
+    {
+        $elemento = $repo->find($id);
+
+        $form = $this->createForm(EseguitoType::class, $elemento);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $elemento = $form->getData();
+            
+            $em->persist($elemento);
+            $em->flush();
+
+            $this->addFlash('success', 'I dati della scadenza eseguita sono stati modificati');
+            
+            return $this->redirectToRoute('app_eseguito_lista', [
+                'id' => $elemento->getId(),
+            ]);
+        }
+
+        if($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', "Ci sono degli errori nella modifica della scadenza eseguita");
+        }
+
+        return $this->render('eseguito/modifica.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/eseguito/cancella/{id}/ok', name: 'app_eseguito_cancella_ok')]
+    public function Cancella($id, EseguitoRepository $repo, EntityManagerInterface $em): Response
+    {
+        $elemento = $repo->findOneBy(['id' => $id]);
+
+        $em->remove($elemento);
+        $em->flush();
+        
+        return $this->redirectToRoute('app_eseguito_lista');
+    }
+
+    #[Route('/admin/eseguito/mostra/{id}', name: 'app_eseguito_mostra')]
+    public function Mostra($id, EseguitoRepository $repo, EntityManagerInterface $em): Response
+    {
+        $elemento = $repo->findOneBy(['id' => $id]);        
+
+        return $this->render('eseguito/mostra.html.twig', [
+            'elemento' => $elemento,
+        ]);
+    }
+}
---
+++ src/Form/EseguitoType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Eseguito;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EseguitoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('Attivita', TextType::class, [
+            'label' => 'Attività', 
+            'label_attr' => ['class' => 'form-label'],
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Inserire l\'attività'
+            ]
+        ])
+        ->add('DataScadenza', DateType::class, [
+            'widget' => 'single_text',
+            'label' => 'Scadenza', 
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control',
+            ]
+        ]) 
+        ->add('DataEseguito', DateType::class, [
+            'widget' => 'single_text',
+            'label' => 'Fatto il', 
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control',
+            ]
+        ]) 
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Eseguito::class,
+        ]);
+    }
+}
