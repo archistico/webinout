e970f4d83f1cb4a215dddcd56ce58f3e493f4a69
Emilie Rollandin <info@archistico.com>
2024-01-13 11:47:55 +0100

    Controller e type scadenza

---
+++ src/Controller/ScadenzaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\MacroType;
+use App\Repository\ScadenzaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ScadenzaController extends AbstractController
+{
+    #[Route('/admin/scadenza', name: 'app_scadenza_lista')]
+    public function Lista(ScadenzaRepository $repo): Response
+    {
+        $elementi = $repo->findAll();
+
+        return $this->render('scadenza/lista.html.twig', [
+            'lista' => $elementi,
+        ]);
+    }
+
+    #[Route('/admin/scadenza/nuovo', name: 'app_scadenza_nuovo')]
+    public function Nuovo(Request $request, EntityManagerInterface $em): Response
+    {
+        $elemento = null;
+        $form = $this->createForm(MacroType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $elemento = $form->getData();
+            
+            $em->persist($elemento);
+            $em->flush();
+            
+            $this->addFlash('success', "Una nuova scadenza è stata aggiunta");
+
+            return $this->redirectToRoute('app_scadenza_lista');
+        }
+
+        if($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', "Ci sono degli errori nell'inserimento della scadenza");
+        }
+
+        return $this->render('scadenza/nuovo.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/scadenza/modifica/{id}', name: 'app_scadenza_modifica')]
+    public function Modifica($id, Request $request, EntityManagerInterface $em, ScadenzaRepository $repo): Response
+    {
+        $elemento = $repo->find($id);
+
+        $form = $this->createForm(MacroType::class, $elemento);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $elemento = $form->getData();
+            
+            $em->persist($elemento);
+            $em->flush();
+
+            $this->addFlash('success', 'I dati della scadenza sono stati modificati');
+            
+            return $this->redirectToRoute('app_scadenza_lista', [
+                'id' => $elemento->getId(),
+            ]);
+        }
+
+        if($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', "Ci sono degli errori nella modifica della scadenza");
+        }
+
+        return $this->render('scadenza/modifica.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/scadenza/cancella/{id}/ok', name: 'app_scadenza_cancella_ok')]
+    public function Cancella($id, ScadenzaRepository $repo, EntityManagerInterface $em): Response
+    {
+        $elemento = $repo->findOneBy(['id' => $id]);
+
+        $em->remove($elemento);
+        $em->flush();
+        
+        return $this->redirectToRoute('app_scadenza_lista');
+    }
+}
---
+++ src/Form/ScadenzaType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Scadenza;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ScadenzaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Attivita', TextType::class, [
+                'label' => 'Attività', 
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Inserire l\'attività'
+                ]
+            ])
+            ->add('DataScadenza', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Scadenza', 
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ]) 
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Scadenza::class,
+        ]);
+    }
+}
