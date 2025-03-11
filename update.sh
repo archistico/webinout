5a24c8dcd4dcd7ba3a70cd91f803bccb255d6ef2
Emilie Rollandin <info@archistico.com>
2025-03-11 11:11:46 +0100

    FEAT(update): aggiunto lancio migrazioni

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ rm -rf /home/sites/inout/var/log
 rm -rf /home/sites/inout/var/cache
 git pull
 composer install
+php bin/console doctrine:migration:migrate --no-interaction
 php bin/console asset-map:compile
