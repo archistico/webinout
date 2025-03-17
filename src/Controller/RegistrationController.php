f3b5fe81f8dd554882e02f3be5166543f7175b8d
Emilie Rollandin <emilie.rollandin@gmail.com>
2025-03-17 13:54:57 +0000

    Tolto autenticazione

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,31 +13,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    //#[Route('/register', name: 'app_register')]
-    //public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    //{
-    //    $user = new User();
-    //    $form = $this->createForm(RegistrationFormType::class, $user);
-    //    $form->handleRequest($request);
-//
-    //    if ($form->isSubmitted() && $form->isValid()) {
-    //        // encode the plain password
-    //        $user->setPassword(
-    //            $userPasswordHasher->hashPassword(
-    //                $user,
-    //                $form->get('plainPassword')->getData()
-    //            )
-    //        );
-//
-    //        $entityManager->persist($user);
-    //        $entityManager->flush();
-    //        // do anything else you need here, like send an email
-//
-    //        return $this->redirectToRoute('app_home');
-    //    }
-//
-    //    return $this->render('registration/register.html.twig', [
-    //        'registrationForm' => $form->createView(),
-    //    ]);
-    //}
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+       $user = new User();
+       $form = $this->createForm(RegistrationFormType::class, $user);
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid()) {
+           // encode the plain password
+           $user->setPassword(
+               $userPasswordHasher->hashPassword(
+                   $user,
+                   $form->get('plainPassword')->getData()
+               )
+           );
+
+           $entityManager->persist($user);
+           $entityManager->flush();
+           // do anything else you need here, like send an email
+
+           return $this->redirectToRoute('app_home');
+       }
+
+       return $this->render('registration/register.html.twig', [
+           'registrationForm' => $form->createView(),
+       ]);
+    }
 }
